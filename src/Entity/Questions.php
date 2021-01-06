544eea561a950a4564293d8b1cf8d0f6847e7b83
mariodevweb <mariojosech@gmail.com>
2021-01-06 15:52:06 +0100

    First commit

---
+++ src/Entity/Questions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=QuestionsRepository::class)
+ */
+class Questions
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
